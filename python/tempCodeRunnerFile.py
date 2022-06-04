151df3eee82932a1fd12fdaf0d76f47e3551d272
Sachin Sharma <sachinsharma2168@gmail.com>
2022-06-04 19:21:40 +0530

    commit

--- python/tempCodeRunnerFile.py
+++ python/tempCodeRunnerFile.py
@@ -1,9 +1,1 @@
-import os
 
-print("directory path:",os.getcwd())
-os.chdir("my directory")
-print("current working directory", os.getcwd())
-os.rename("sachin.txt","oops/txt")
-print("file renamed")
-os.removed("sachin.txt")
-print("file removed from directory")
